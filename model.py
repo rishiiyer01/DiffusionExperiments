fixed softmax over hermitian inner product error

model.py
@@ -9,17 +9,19 @@ import torch.nn.functional as F
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self,d_model,image):
+    def __init__(self,d_model):
         super(PositionalEncoding,self).__init__()
+        
+        self.linear=nn.Linear(1,d_model,bias=False)
+    def forward(self,image):
         b,c,h,w=image.shape
         xfreqs=torch.fft.fftfreq(h,dtype=torch.float)
         yfreqs=torch.fft.fftfreq(w,dtype=torch.float)
         freqspace=torch.einsum('i,j->ij',xfreqs,yfreqs)
-        self.freqs=freqspace.unsqueeze(0)
-        self.linear=nn.Linear(c,d_model)
-    def forward(self,image):
-        # for now no positional encoding
-        pass
+        freqs=freqspace.unsqueeze(0)
+        x=self.linear(freqs)
+
+        return x
 
         
 
@@ -66,11 +68,11 @@ class spectralModel(nn.Module):
 
 
 class ComplexSoftmax(nn.Module):
-    def __init__(self, use_phase=True):
+    def __init__(self, use_phase=False):
         super(ComplexSoftmax,self).__init__()
         # act can be either a function from nn.functional or a nn.Module if the
         # activation has learnable parameters
-        self.act = nn.Softmax(dim=-1)
+        self.act = nn.Softmax(dim=1)
         self.use_phase = use_phase
 
     def forward(self, z):
@@ -163,11 +165,11 @@ class spectralTransformerBlock(nn.Module):
         self.weights_q = nn.Parameter(torch.rand(in_channels, out_channels, dtype=torch.cfloat))
         self.weights_k = nn.Parameter(torch.rand(in_channels, out_channels, dtype=torch.cfloat))
         self.weights_v = nn.Parameter(torch.rand(in_channels, out_channels, dtype=torch.cfloat))
-
+        #self.pos_encoding=PositionalEncoding(out_channels,x)
         # Output projection
         self.weights_o = nn.Parameter(torch.rand(out_channels, out_channels, dtype=torch.cfloat))
 
-        self.softmax = ComplexSoftmax()
+        self.softmax = nn.Softmax(dim=1)
         self.rms_norm = nn.RMSNorm([in_channels])
 
     def generate_causal_mask(self, size):
@@ -190,7 +192,7 @@ class spectralTransformerBlock(nn.Module):
         x=self.rms_norm(x)
         
         x=x.permute(0,3,1,2)
-        
+        #pos_enc=self.pos_encoding(x)
         xfft=torch.fft.rfft2(x,norm='ortho')
         b, c, hft, wft = xfft.shape
         hw = hft * wft
@@ -202,7 +204,7 @@ class spectralTransformerBlock(nn.Module):
         
         
         freqs = xfft.reshape(b, c, hw)
-
+        #freqs+=pos_enc #adding positional encoding
         # Compute Q, K, V
         q = torch.einsum("bix,io->box", freqs, self.weights_q)
         k = torch.einsum("bix,io->box", freqs, self.weights_k)
@@ -215,7 +217,11 @@ class spectralTransformerBlock(nn.Module):
         v = self.split_heads(v)
         
         # Compute attention scores
-        s = torch.einsum("bnix,bniy->bnxy", q, k.conj()) * self.scale
+        s = torch.einsum("bnix,bniy->bnxy", q.conj(), k) * self.scale 
+        
+        # it should be noted here that the scores here are complex valued, to associate them with real probabilities we take the square of the absolute value or softmax the absolute value
+        s=torch.abs(s)
+        
         
         # Apply causal mask
         causal_mask = self.generate_causal_mask(hw).to(s.device)
@@ -226,7 +232,7 @@ class spectralTransformerBlock(nn.Module):
         # Apply softmax
         s = self.softmax(s)
         
-        s = s.masked_fill(causal_mask, 0)
+        s = s.masked_fill(causal_mask, 0).to(torch.cfloat)
 
         
 
@@ -246,9 +252,5 @@ class spectralTransformerBlock(nn.Module):
 
         return out
 
-
 model=spectralModel(16,16,1)
 
-x=torch.randn(2,16,16,16)
-out=model(x)
-print(out[0])